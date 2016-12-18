c6c645b5f0c9438bf117d7035b7aef2b83036127
Tim Schraepen <sch3lp@gmail.com>
2016-12-18 15:49:41 +0100

    showInfo working

--- frontend/src/Home.elm
+++ frontend/src/Home.elm
@@ -6,9 +6,8 @@ import Html.Events exposing (..)
 
 
 -- Define your messages
+type Msg = ShowInfo 
 -- Setup your model
-
-
 type alias Model =
     { showProfileText : Bool
     , currentPet :
@@ -20,17 +19,76 @@ type alias Model =
         }
     }
 
-
-
 -- initialModel =
--- ...
+initialModel: Model 
+initialModel =
+    { showProfileText = False
+    , currentPet =
+        { id = 1
+        , name = "BellyBell"
+        , distance = 24
+        , text = "Look at this face. Is this not the derpiest face you have ever seen? This is Bluebell, the chicken who acts like a dog and thinks she’s people. Bluebell’s you’re in the backyard relaxing in your lounge chair, you can count on Bluebell to be nearby, napping in the grass at your feet."
+        , photoUrl = "http://localhost:3000/profiles/bluebell1.jpg"
+        }
+    }
+
 -- Define an update function
--- ...
+update: Msg -> Model -> Model
+update msg model = 
+    case msg of
+        ShowInfo ->
+        { model | showProfileText = not model.showProfileText }
+
+
 -- Define a view function
--- ...
+view: Model -> Html Msg
+view model = 
+    div []
+    [ header []
+        [ span [ class "header-title" ]
+            [ text "Petindr" ]
+        , button [ class "icon-right chat-icon" ]
+            []
+        ]
+    , div [ class "container main-container" ]
+        [ div [ class "profiles" ]
+            [ div [ class "profile" ]
+                [ div []
+                    [ img [ src model.currentPet.photoUrl ]
+                        []
+                    , if model.showProfileText then
+                        div [ class "profile-text" ]
+                            [ text model.currentPet.text ]
+                      else
+                        div [] []
+                    ]
+                , div [ class "identification" ]
+                    [ span [ class "identification-name" ]
+                        [ text model.currentPet.name ]
+                    , span [ class "identification-distance" ]
+                        [ text ((toString model.currentPet.distance)++"km") ]
+                    ]
+                ]
+            ]
+        , div [ class "button-group" ]
+            [ button [ class "button-round button-primary button-big icon-x" ]
+                [ img [ src "/styling/images/x-icon.png" ]
+                    []
+                ]
+            , button [ class "button-round button-primary button-small button-front", onClick ShowInfo ]
+                [ img [ src "/styling/images/i-icon.png" ]
+                    []
+                ]
+            , button [ class "button-round button-primary button-big" ]
+                [ img [ src "/styling/images/like-icon.png" ]
+                    []
+                ]
+            ]
+        ]
+    ]
+
+
 -- main
-
-
 main =
     beginnerProgram
         { model = initialModel
