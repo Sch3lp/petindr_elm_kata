df026c2dd9aa3df1a8148bd43c192aa2606f7692
= <=>
2017-11-02 20:55:54 +0100

    added advancePet tests

--- frontend/tests/HomeTests.elm
+++ frontend/tests/HomeTests.elm
@@ -1,6 +1,7 @@
 module HomeTests exposing (..)
 
 import Home exposing (..)
+import Pets exposing (princess, cricket, babyChicken)
 import Expect exposing (Expectation)
 import Test exposing (..)
 import Html exposing (..)
@@ -30,4 +31,79 @@ conditionallyRenderTest =
                     conditionallyRender toBeRenderedDiv True
                         |> Query.fromHtml
                         |> Query.contains [ toBeRenderedDiv ]
+        ]
+
+
+advancePetTest : Test
+advancePetTest =
+    describe "advancePetTest"
+        [ describe "model.nextPets tests"
+            [ test "nextPets is empty, nextPets remains empty" <|
+                \_ ->
+                    let
+                        nextPets =
+                            []
+
+                        model =
+                            Model False princess nextPets False princess
+
+                        actual =
+                            advancePet model
+                    in
+                        Expect.equal [] actual.nextPets
+            , test "nextPets contains one value, nextPets becomes empty" <|
+                \_ ->
+                    let
+                        nextPets =
+                            [ cricket ]
+
+                        model =
+                            Model False princess nextPets False princess
+
+                        actual =
+                            advancePet model
+                    in
+                        Expect.equal [] actual.nextPets
+            , test "nextPets contains multiple values, nextPets is trimmed from the top" <|
+                \_ ->
+                    let
+                        nextPets =
+                            [ cricket, princess ]
+
+                        model =
+                            Model False princess nextPets False princess
+
+                        actual =
+                            advancePet model
+                    in
+                        Expect.equal [ princess ] actual.nextPets
+            ]
+        , describe "model.currentPet tests"
+            [ test "nextPets is empty, currentPet is defaulted to Princess" <|
+                \_ ->
+                    let
+                        nextPets =
+                            []
+
+                        model =
+                            Model False cricket nextPets False cricket
+
+                        actual =
+                            advancePet model
+                    in
+                        Expect.equal princess actual.currentPet
+            , test "nextPets contains a pet, currentPet is set to this last pet" <|
+                \_ ->
+                    let
+                        nextPets =
+                            [babyChicken]
+
+                        model =
+                            Model False cricket nextPets False cricket
+
+                        actual =
+                            advancePet model
+                    in
+                        Expect.equal babyChicken actual.currentPet
+            ]
         ]
